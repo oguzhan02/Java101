02fdc46b86fec33c423f1ca1fa5ce1a607622253
Oguzhan <oguz_dmr69@hotmail.com>
2024-12-02 20:19:20 +0300

    CircleArea calculator added

---
+++ src/CircleArea.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CircleArea {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter radius of the circle:");
+        double radius=input.nextDouble();
+        System.out.println("Enter angle of the circle segment");
+        double angle=input.nextDouble();
+        double pi=3.14;
+        double area=0;
+        area=(pi*radius*radius)*(angle/360);
+        System.out.println("Area of circle sector is "+area);
+
+    }
+}
