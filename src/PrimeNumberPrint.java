329e64f683de2f8ede7bcb5ebc79dfcfff59c3b3
Oguzhan <oguz_dmr69@hotmail.com>
2024-12-04 15:05:07 +0300

    A program that prints all prime numbers between 1 and 100

---
+++ src/PrimeNumberPrint.java
@@ -0,0 +1,34 @@
+public class PrimeNumberPrint {
+    public static void main(String[] args){
+// this one more than second
+        for (int i = 2; i <100; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+/*
+        for (int i=2;i<100;i++){
+            int x=2;
+            while (x<=i){
+                if(i%x==0 )break;
+                x++;
+            }
+            if(x==i) System.out.println(i);
+        }
+
+ */
+
+
+
+
+
+
+    }
+}
